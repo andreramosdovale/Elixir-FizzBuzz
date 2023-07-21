ba96954080867506292f94c14d3555341cacb6d3
André Ramos <andre_ramosdovale@outlook.com>
2023-07-20 23:05:20 -0300

    feat: create mock file and create function to read file

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,18 +1,22 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def buildCase(file_name) do
+    case File.read(file_name) do
+      {:ok, file} -> file
+      {:error, reason} -> reason
+    end
   end
+
+  def buildPattern(file_name) do
+    file = File.read(file_name)
+    handle_file_read(file)
+  end
+
+  def buildPipe(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
 end
